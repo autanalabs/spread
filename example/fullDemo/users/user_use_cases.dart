9c463ea12526a91c85a1f93383908cce3c0ea591
gb <gb@autanalabs.com>
2023-08-14 17:32:20 -0300

    update example

--- example/fullDemo/users/user_use_cases.dart
+++ example/fullDemo/users/user_use_cases.dart
@@ -1,24 +1,33 @@
 
 import 'package:spread/spread_emitter_mixin.dart';
+import 'package:spread/spread_state.dart';
 import 'package:spread/use_case.dart';
 import '../services.dart';
+import 'user.dart';
 import 'user_states.dart';
 
 class LoadUsersUseCase with StateEmitter implements UseCase {
 
   @override
   void execute() async {
-    emit<UsersState>(LoadingUsers());
-    Services().userService.getUsers()
-    .then((users) {
-      emit<UsersState>(LoadedUsersSuccess(users: users));
-    })
-    .onError((error, stackTrace) {
-      emit<UsersState>(LoadedUsersFail(
-          error: error,
-          stackTrace: stackTrace
-      ));
-    });
+    final List<User>? usersCached = SpreadState().getNamed<List<User>>(
+        "users_cache");
+    if (usersCached != null) {
+      emit<UsersState>(LoadedUsersSuccess(users: usersCached));
+    } else {
+      emit<UsersState>(LoadingUsers());
+      Services().userService.getUsers()
+          .then((users) {
+        emitNamed("users_cache", users);
+        emit<UsersState>(LoadedUsersSuccess(users: users));
+      })
+          .onError((error, stackTrace) {
+        emit<UsersState>(LoadedUsersFail(
+            error: error,
+            stackTrace: stackTrace
+        ));
+      });
+    }
   }
 }
 
